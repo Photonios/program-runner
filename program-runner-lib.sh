949e2c5e25808cd26387565ffa4134af98e4a7e1
Photonios <photonios@outlook.com>
2016-01-15 12:53:45 +0200

    Make sure that program is running before trying to send signal

--- program-runner-lib.sh
+++ program-runner-lib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function get_pid {
-    psaux_line=`ps aux | grep program-runner | grep $1`
+    psaux_line=`ps aux | grep program-runner | grep $1 | grep -v bash`
+    name=`echo $psaux_line | awk '{print $12'}`
     pid=`echo $psaux_line | awk '{print $2}'` 
 }
